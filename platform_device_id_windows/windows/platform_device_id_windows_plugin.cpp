d50cfd7c4b16f6e745ed1096e63cb54aef3e64ff
matif <mateuszfilipek2000@gmail.com>
2023-11-12 13:14:15 +0100

    feat: replace wmic csproduct lookup on windows with registry check

--- platform_device_id_windows/windows/platform_device_id_windows_plugin.cpp
+++ platform_device_id_windows/windows/platform_device_id_windows_plugin.cpp
@@ -15,70 +15,91 @@
 #include <memory>
 #include <sstream>
 
-namespace {
+namespace
+{
 
-class PlatformDeviceIdWindowsPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  class PlatformDeviceIdWindowsPlugin : public flutter::Plugin
+  {
+  public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  PlatformDeviceIdWindowsPlugin();
+    PlatformDeviceIdWindowsPlugin();
 
-  virtual ~PlatformDeviceIdWindowsPlugin();
+    virtual ~PlatformDeviceIdWindowsPlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+  private:
+    // Called when a method is called on this plugin's channel from Dart.
+    void HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  };
 
-// static
-void PlatformDeviceIdWindowsPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "com.di1shuai.flutter/platform_device_id",
-          &flutter::StandardMethodCodec::GetInstance());
+  // static
+  void PlatformDeviceIdWindowsPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarWindows *registrar)
+  {
+    auto channel =
+        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+            registrar->messenger(), "com.di1shuai.flutter/platform_device_id",
+            &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<PlatformDeviceIdWindowsPlugin>();
+    auto plugin = std::make_unique<PlatformDeviceIdWindowsPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    channel->SetMethodCallHandler(
+        [plugin_pointer = plugin.get()](const auto &call, auto result)
+        {
+          plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
 
-  registrar->AddPlugin(std::move(plugin));
-}
-
-PlatformDeviceIdWindowsPlugin::PlatformDeviceIdWindowsPlugin() {}
-
-PlatformDeviceIdWindowsPlugin::~PlatformDeviceIdWindowsPlugin() {}
-
-void PlatformDeviceIdWindowsPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  FILE *rstream;
-  std::string deviceId;
-  std::string deviceIdResult;
-  char buf[1024] = {0};
-
-  if (method_call.method_name().compare("getDeviceId") == 0) {
-    rstream = _popen("wmic csproduct get UUID","r");
-    fread(buf, sizeof(char), sizeof(buf), rstream);
-    _pclose(rstream);
-    deviceIdResult = buf;
-    std::size_t pos = deviceIdResult.find("\n");
-    deviceId = deviceIdResult.substr(pos+1);
-    result->Success(flutter::EncodableValue(deviceId));
-  } else {
-    result->NotImplemented();
+    registrar->AddPlugin(std::move(plugin));
   }
-}
 
-}  // namespace
+  PlatformDeviceIdWindowsPlugin::PlatformDeviceIdWindowsPlugin() {}
+
+  PlatformDeviceIdWindowsPlugin::~PlatformDeviceIdWindowsPlugin() {}
+
+  void PlatformDeviceIdWindowsPlugin::HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
+    if (method_call.method_name().compare("getDeviceId") == 0)
+    {
+      HKEY hKey;
+      char szBuffer[512];
+      DWORD dwBufferSize = sizeof(szBuffer);
+      LONG lRet;
+
+      lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+                          TEXT("SOFTWARE\\Microsoft\\Cryptography"),
+                          0, KEY_READ, &hKey);
+
+      if (lRet == ERROR_SUCCESS)
+      {
+        lRet = RegQueryValueEx(hKey, TEXT("MachineGuid"), NULL, NULL,
+                               (LPBYTE)szBuffer, &dwBufferSize);
+        if (lRet == ERROR_SUCCESS)
+        {
+          std::string deviceId = szBuffer;
+          result->Success(flutter::EncodableValue(deviceId));
+        }
+        RegCloseKey(hKey);
+      }
+      else
+      {
+        result->Error("UNAVAILABLE", "Device id is not available.");
+      }
+    }
+    else
+    {
+      result->NotImplemented();
+    }
+  }
+
+} // namespace
 
 void PlatformDeviceIdWindowsPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+    FlutterDesktopPluginRegistrarRef registrar)
+{
   PlatformDeviceIdWindowsPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
           ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
